d188c3eecb135e71edffb9c90bc7656194886758
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2026-03-07 01:11:47 +0530

    feat: Implement a basic calculator in JavaScript using a switch statement.

---
+++ Brad Traversy/Basic_Coding/calculator.js
@@ -0,0 +1,21 @@
+let num1 = 10;
+let num2 = 20;
+
+let operator = "+";
+
+switch (operator) {
+  case "+":
+    console.log(num1 + num2);
+    break;
+  case "-":
+    console.log(num1 - num2);
+    break;
+  case "*":
+    console.log(num1 * num2);
+    break;
+  case "/":
+    console.log(num1 / num2);
+    break;
+  default:
+    console.log("Invalid operator");
+}
